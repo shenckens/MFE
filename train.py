bbf9f62756e28e9ffc3355459f477475599620ca
shenckens <stijn.henckens@gmai.com>
2022-04-29 09:05:51 +0200

    first run

--- train.py
+++ train.py
@@ -12,6 +12,24 @@ import argparse
 
 datapath = '/project/henckens/data/scannet'
 
+
+def evaluate(model, recon_img, gt_img, mask, epoch):
+
+    input = fill_recon_img(recon_img, gt_img, mask)
+    input = torch.unsqueeze(input, dim=1)
+    input = input.to(device=device, dtype=torch.float)
+    gt_img = torch.unsqueeze(gt_img, dim=1)
+    gt_img = gt_img.to(device=device, dtype=torch.float)
+
+    # forward pass
+    output = model(input)
+
+    # calculate loss
+    loss = loss_module(output, gt_img)
+
+    return loss
+
+
 if __name__ == "__main__":
 
     # ----------------------------------------
@@ -31,6 +49,8 @@ if __name__ == "__main__":
                         help='The maximum value (in meters) from which the depth is not counted and set to 0.')
     parser.add_argument('--loss_fn', type=str, default='l1',
                         help="The loss function used (either 'ssim' or 'l1')")
+    parser.add_argument('--save_model', type=bool, default=True,
+                        help='Boolean to indicate if the model parameters should be saved to disk after every epoch.')
     # weight decay?
     args = parser.parse_args()
     print(args)
@@ -38,10 +58,14 @@ if __name__ == "__main__":
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(f'Using {device} device.')
 
-    # train_data = NeuconDepths('./Desktop/data', 'test')
+    # Data and DataLoaders
+    # train_data = NeuconDepths('./Desktop/data', 'test') # for local testing
     train_data = TestsetNeuconDepths(datapath, 'train', zclip=args.zclip)
-    # valdata = TestsetNeuconDepths('./Desktop/data', 'val', zclip=args.zclip)
+    val_data = TestsetNeuconDepths('./Desktop/data', 'val', zclip=args.zclip)
     train_dl = DataLoader(train_data, batch_size=args.batch_size, shuffle=True)
+    val_dl = DataLoader(val_data, batch_size=args.batch_size, shuffle=False)
+
+    # Model and optimizer
     model = Unet(args.base_channel_size)
     model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
@@ -52,7 +76,8 @@ if __name__ == "__main__":
     elif arg.loss_fn == 'ssim':
         loss_module = pytorch_ssim.SSIM()
 
-    epoch_loss = []
+    train_loss = []
+    val_loss = []
     for epoch in range(args.epochs):
         losses = []
         print(f'Epoch {epoch+1}/{args.epochs}')
@@ -83,11 +108,23 @@ if __name__ == "__main__":
                 print(f'Completed {i}/{len(train_dl)} iterations\
                       on epoch {epoch+1}/{args.epochs}')
 
-        epoch_loss.append(losses.mean())
-        print(f'Completed one epoch')
-        break
-        model.eval()
-        # do validation
-        # update graph
+        train_loss.append(losses.mean())
 
-    print(epoch_loss)
+        # Validation
+        losses_val = []
+        model.eval()
+        for recon_img, gt_img, mask in val_dl:
+            loss_val = evaluate(model, recon_img, gt_img, mask, epoch)
+            losses_val.append(loss_val)
+        val_loss.append(losses_val.mean())
+
+        print(f'Completed epoch {epoch+1}.')
+        print(f'Training loss: {train_loss}')
+        print(f'Validation loss: {val_loss}')
+
+        # Saving model so far.
+        torch.save(model.state_dict(), './saved_parameters/{}_epoch{}_lr{}_bs{}_zclip{}'.format(
+            model.__class__.__name__, epoch+1, args.lr, args.batch_size, args.zclip))
+
+    print(f'Train loss per epoch {train_loss}')
+    print(f'Validation loss per epoch {val_loss}')
