2e8319b3cd3946fbb8cf1ce3bea95246e2613bd5
shenckens <stijn.henckens@gmai.com>
2022-05-12 13:20:39 +0200

    update utils

--- utils.py
+++ utils.py
@@ -1,30 +1,39 @@
 import numpy as np
 import torch
-import torch.nn as nn
-
-# Loss functions
-
-
-def SSIM_loss(x, y):
-    loss = 0
-    return loss
-
-
-def L1(x, y):
-    loss = nn.L1Loss(x, y)
-    return loss
-
-
-def L2(x, y):
-    loss = 0
-    return loss
-
-
-def BCE(x, y):
-    loss = 0
-    return loss
 
 
 def fill_recon_img(recon_img, gt_img, mask):
     img = np.where(mask, gt_img, recon_img)
     return torch.from_numpy(img)
+
+
+def stack_patch(patch, img):
+    ''' Takes a mask img of a planar patch and original image.
+        Returns the stacked points of the planar patch as a numpy array.
+    '''
+    pts = []
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            if patch[i][j]:
+                pts.append((i, j, img[i][j]))
+    A = np.stack(pts, axis=0)
+    print(A.shape)
+    return A
+
+
+def calculate_normal(A):
+    '''An = b. Where A is a stacked matrix of 3d points in a patch,
+       n is the normal vector and b a 3d vector of ones.
+    '''
+    b = np.ones((3, 1))
+    n = np.linalg.inv(A.T @ A) @ A.T @ b
+    n /= np.linalg.norm(n, ord=2)
+    return n
+
+
+def MPL(n, n_gt):
+    '''Computes the Mean Planar Loss for one single planar patch
+       in a current and target image.
+    '''
+    loss = np.linalg.norm(n - n_gt, ord=1)
+    return loss
